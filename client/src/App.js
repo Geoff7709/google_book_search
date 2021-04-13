2f6ac0152ee9667e6d3c5950735e91c6720bb4cc
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2021-04-13 18:02:55 -0500

    added header

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,31 @@ import {
 import Search from './pages/search'
 import Saved from './pages/saved'
 import Navbar from './components/navbar'
+import Container from './components/container'
+import Row from './components/row'
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/search">
-          <Search />
-        </Route>
-        <Route path="/">
-          <Saved />
-        </Route>
-      </Switch>
-    </Router>
+    <Container>
+      <Row>
+        <header>
+          <h1>Google Book Search</h1>
+        </header>
+      </Row>
+      <Row>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route path="/">
+              <Saved />
+            </Route>
+          </Switch>
+        </Router>
+      </Row>
+    </Container>
   );
 }
 
